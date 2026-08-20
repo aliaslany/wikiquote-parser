8f83108076be0b955e747ff115fbc52b0e20d3d5
aliaslani <a77aslani@gmail.com>
2026-08-20 13:34:34 +0330

    fix(web): mount React application

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,8 @@
-console.log('OpenQuotes');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles.css';
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><App /></React.StrictMode>
+);
